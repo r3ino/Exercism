9c9b34dcd0cfa84ac74ea2c00a93aa78b9c83363
r3ino <reino.albrecht@protonmail.com>
2025-02-16 00:36:02 +0100

    specified rule #2 and started rule #3

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -8,11 +8,28 @@ def translate(text):
         text += 'ay'
         return text
 
-    for letter in text:
-        if letter not in vowels:
-            consonants += letter
-        else:
-            break
-    remaining_word = text[len(consonants):]
-    return remaining_word + consonants + 'ay'
+    if not text.startswith(first_letters) and not 'qu' in text:
+        for letter in text:
+            if letter not in vowels:
+                consonants += letter
+            else:
+                break
+        remaining_word = text[len(consonants):]
+        return remaining_word + consonants + 'ay'
 
+    if 'qu' in text:
+        position_qu = text.find('qu')
+
+        if text[position_qu - 1] not in vowels:
+            front_part = text[:position_qu + 2]
+            back_part = text[position_qu + 2:]
+
+            return back_part + front_part + 'ay'
+
+        for letter in text:
+            if letter not in vowels:
+                consonants += letter
+            else:
+                break
+        remaining_word = text[len(consonants):]
+        return remaining_word + consonants + 'ay'
