17dfc89dbee28a05e037b8ebf7be011fba133aad
r3ino <reino.albrecht@protonmail.com>
2025-02-15 20:45:29 +0100

    rule 1 and 2 defined

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,18 @@
 def translate(text):
-    pass
+
+    first_letters = ('a','e','i','o','u','xr','yt')
+    vowels = 'aeiou'
+    consonants = ''
+
+    if text.startswith(first_letters):
+        text += 'ay'
+        return text
+
+    for letter in text:
+        if letter not in vowels:
+            consonants += letter
+        else:
+            break
+    remaining_word = text[len(consonants):]
+    return remaining_word + consonants + 'ay'
+
