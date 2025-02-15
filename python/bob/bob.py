5046ab5da0d498737787bc25c05c8d2ef2bcb3af
r3ino <reino.albrecht@protonmail.com>
2025-02-15 13:44:09 +0100

    solved with nested if statements. 2nd iteration

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,13 @@
 def response(hey_bob):
-    pass
+    hey_bob = hey_bob.strip()
+    is_shout = hey_bob.isupper()
+    is_question = hey_bob.endswith('?')
+    if not hey_bob:
+        return 'Fine. Be that way!'
+    if is_shout:
+        if is_question:
+            return "Calm down, I know what I'm doing!"
+        return 'Whoa, chill out!'
+    if is_question:
+        return 'Sure.'
+    return 'Whatever.'
