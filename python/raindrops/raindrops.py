589cd59f1d1323e272319f1a045e5f774d32cd4b
r3ino <reino.albrecht@protonmail.com>
2025-02-15 14:45:28 +0100

    solved 'raindrops' via for

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,10 @@
 def convert(number):
     pass
+
+    for i in range(3):
+        if number % divider[i] == 0:
+            sound += drop_sound[i]
+
+    if not sound:
+        sound = str(number)
+    return sound
